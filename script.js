9ee555b7f2e889d1a0a31f60f09a0a5a3459f3f3
ahmadabbani <ahmdabbanyy@gmail.com>
2024-06-27 14:57:18 +0300

    updates...

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// Image modal functions
 function openImage(imageSrc) {
   var modal = document.getElementById("imageModal");
   var modalImg = document.getElementById("modalImage");
@@ -24,3 +25,69 @@ function closeImage() {
   var modal = document.getElementById("imageModal");
   modal.style.display = "none";
 }
+
+//Sliders
+$(document).ready(function () {
+  var $slider = $(".slider");
+  $slider.slick({
+    autoplay: false,
+    pauseOnHover: false,
+    autoplaySpeed: 3000,
+    fade: true,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    centerMode: false,
+  });
+  $slider.on("afterChange", function () {
+    $slider.slick("slickPlay");
+  });
+
+  $(".Principles-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    pauseOnHover: false,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    centerMode: false,
+    speed: 500,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 275,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
